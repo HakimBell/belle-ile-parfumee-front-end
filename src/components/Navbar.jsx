5fb4d8353c64b997cc4ba6458079f79bfbbe5c77
hakim <belle.ile.parfumee@gmail.com>
2024-05-19 10:14:57 +0200

    add style navbar & header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { MdHomeFilled, MdCategory, MdShop2, MdContacts } from "react-icons/md";
+
 function Navbar({ containerStyles }) {
   return (
     <nav className={`${containerStyles}`}>
-      <NavLink to={"/"}>
-        <div className="FlexCenter gap-x-1">
+      <NavLink to={"/"} className={({isActive}) => isActive ? "active_link" :""}>
+        <div className="flexCenter gap-x-1">
           <MdHomeFilled />
           Home
         </div>
       </NavLink>
-      <NavLink to={"/mens"}>
-        <div className="FlexCenter gap-x-1">
+      <NavLink to={"/mens"} className={({isActive}) => isActive ? "active_link" :""}>
+        <div className="flexCenter gap-x-1">
           <MdCategory />
           Men's
         </div>
       </NavLink>
-      <NavLink to={"/womens"}>
-        <div className="FlexCenter gap-x-1">
+      <NavLink to={"/womens"} className={({isActive}) => isActive ? "active_link" :""}>
+        <div className="flexCenter gap-x-1">
           <MdShop2 />
           Women's
         </div>
       </NavLink>
-      <NavLink to={"/kids"}>
-        <div className="FlexCenter gap-x-1">
+      <NavLink to={"/kids"}className={({isActive}) => isActive ? "active_link" :""}>
+        <div className="flexCenter gap-x-1">
           <MdContacts />
           Kid's
         </div>
